b15784baa054e0a991f037149a98d367ebbc2955
Tim Lestander <timle211@student.liu.se>
2018-07-04 20:29:25 +0200

    First version of poll list component

---
+++ src/app/components/poll-list/poll-list.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { PollInterface } from '../../interfaces/poll.interface';
+import { OptionInterface } from '../../interfaces/option.interface';
+import { VoteInterface } from '../../interfaces/vote.interface';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-poll-list',
+  templateUrl: './poll-list.component.html',
+  styleUrls: ['./poll-list.component.css']
+})
+export class PollListComponent implements OnInit {
+  public openPolls: PollInterface[] = [];
+  public closedPolls: PollInterface[] = [];
+
+  constructor(
+    private apiService: ApiService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.apiService.getAllPolls().subscribe((polls: PollInterface[]) => {
+      this.extractData(polls);
+    });
+  }
+
+  public extractData(polls: PollInterface[]): void {
+    polls.forEach((poll: PollInterface) => {
+      let totalVotes: number = 0;
+      let found: boolean = false;
+      poll.options.forEach((option: OptionInterface) => {
+        option.votes.forEach((vote: VoteInterface) => {
+          if (vote.userId === this.authService.user.id) {
+            found = true;
+          }
+          totalVotes++;
+        });
+      });
+      poll['totalVotes'] = totalVotes;
+      found ? this.closedPolls.push(poll) : this.openPolls.push(poll);
+    });
+  }
+
+  public getVoteCount(options: OptionInterface[]): number {
+    let total = 0;
+    options.forEach((option: OptionInterface) => {
+      total += option.votes.length;
+    });
+    return total;
+  }
+
+  public goToPoll(pollId: number): void {
+    this.router.navigateByUrl(`/poll/${pollId}`);
+  }
+}
