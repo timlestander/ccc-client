83ac562cc8bb1507109cc24dd991fde0f20e8157
Tim Lestander <timle211@student.liu.se>
2018-07-08 23:25:14 +0200

    Updated seed with hashed passwords

--- server/seeds/user.js
+++ server/seeds/user.js
@@ -6,38 +6,38 @@ module.exports = () => {
       id: 100,
       name: 'Tim Lestander',
       username: 'tim',
-      password: 'tim',
+      password: '$2a$04$cRf93fohCBmADnjhwKVAiO7Ei5MjPpPigJFbfzerm0eZJXN0teIMq',
       hh: true
     },
     {
       id: 101,
       name: 'Jakob Nilsson',
       username: 'jakob',
-      password: 'jakob'
+      password: '$2a$04$rlggLm0CBv.IM2DH8CAA2.oOO1ek9VflJLd9dtAVzzWwhV0Nxi3Xm'
     },
     {
       id: 102,
       name: 'Jakob Danielsson',
       username: 'babben',
-      password: 'babben'
+      password: '$2a$04$mxXoeA9aNyAzy9eSErKYJua1wnQyRO93SFkfzHIpRoiGgrp2yE2nW'
     },
     {
       id: 103,
       name: 'Christoffer Johansson',
       username: 'chrisse',
-      password: 'chrisse'
+      password: '$2a$04$gj4uV9sL6bHtJPo2oO/kCOvD0Jv6h6eHkJrNsG0S2rUsm0rlSJzve'
     },
     {
       id: 104,
       name: 'Benny Lam',
       username: 'benny',
-      password: 'benny'
+      password: '$2a$04$GLMQ8QMInPLqP.MyH4liEe7ZudbFzojK0.H4KMbHH6mFpOYgRDt7i'
     },
     {
       id: 105,
       name: 'Marcus',
       username: 'marcus',
-      password: 'marcus'
+      password: '$2a$04$5fDHaTP7IZTxEsO1ZaqnnOSM.2r9.yncZwIGhRRKCSQEnWbZz1klK'
     }
   ]);
 };
