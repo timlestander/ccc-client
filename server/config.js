addea5db3911aeba035e7af8851fccec33891b71
Tim Lestander <timle211@student.liu.se>
2018-07-08 14:17:32 +0200

    Updated to use environment port

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  port: 3000,
+  port: process.env.PORT || 3000,
   dbConnectionString: 'postgres://postgres:postgres@localhost:5432/ccc', // CHANGE BEFORE PUSHING, I HAVE OTHER USER THAN TIM
   // dbConnectionString: 'postgres://ola:postgres@localhost:5432/ccc', // CHANGE BEFORE PUSHING, I HAVE OTHER USER THAN TIM
   saltRounds: 2,
