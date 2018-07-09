de262be9abfd5f979e230bb434438102ef881d7b
Tim Lestander <timle211@student.liu.se>
2018-07-09 20:31:15 +0200

    Ugly version of spinning circle of life

--- src/app/components/overview/overview.component.ts
+++ src/app/components/overview/overview.component.ts
@@ -44,8 +44,15 @@ export class OverviewComponent implements OnInit {
     }
   }
 
-  /*public drawCircle() {
-    const footer = document.querySelector('ul');
-    footer.setAttribute('class', 'circle-container');
-  }*/
+  public getPosition(item: number, length: number): Object {
+    const angle: number = (360 / length) * item;
+    const y: number = Math.sin((angle * Math.PI) / 180);
+    const x: number = Math.cos((angle * Math.PI) / 180);
+
+    return {
+      position: 'relative',
+      top: y * 130 + 'px',
+      left: x * 130 + 'px'
+    };
+  }
 }
